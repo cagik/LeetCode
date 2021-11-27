ae79d9121479051ec4e51bae7c9e56e90807199b
Li Yiming <liyiming_cagik@qq.com>
2021-11-27 21:55:15 +0800

    Update and rename 2.cpp to 2两数相加.cpp

--- leetcode/2两数相加.cpp
+++ leetcode/2两数相加.cpp
@@ -1,3 +1,6 @@
+/*
+这个写法和官方题解思路其实是一样的，但是废话比较多使得更容易理解一点
+*/
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -12,25 +15,25 @@ using namespace std;
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* l3 = new ListNode(0);
-        ListNode* l3_tmp = l3;
+        ListNode* l3 = new ListNode(0); //开一个新的链表L3， 用于储存结果。
         
+        ListNode* l3_tmp = l3; //使用tmp在链表中移动
         ListNode* l1_tmp = l1;
         ListNode* l2_tmp = l2;
 
-        int num1 =0, num2=0, total=0;
+        int num1 =0, num2=0, total=0; //num1、num2代表两个链表当前位置上的数字， total表示进位加上两数字的和
 
-        bool flag = false;
+        bool flag = false;//flag表示是否存在进位
 
-        while(l1_tmp != NULL || l2_tmp != NULL){
-            if(l1_tmp!=NULL){
+        while(l1_tmp != NULL || l2_tmp != NULL){     //执行遍历，当两个链表不同时遍历到末尾时进行遍历
+            if(l1_tmp!=NULL){                        //如果链表1没有遍历至末尾，取出当前数字，并向后移动；
                 num1 = l1_tmp->val;
                 l1_tmp = l1_tmp->next;
             }
-            else{
+            else{                                    //否则，将num1置0，同时不移动L1
                 num1 = 0;
             }
-            if(l2_tmp!=NULL){
+            if(l2_tmp!=NULL){                        //与前面同理
                 num2 = l2_tmp->val;
                 l2_tmp = l2_tmp->next;
             }
@@ -38,7 +41,7 @@ public:
                 num2 = 0;
             }
 
-            total = flag;
+            total = flag;                          
             
             if(total + num1+num2 >= 10){
                 total = total + num2 + num1 -10;
